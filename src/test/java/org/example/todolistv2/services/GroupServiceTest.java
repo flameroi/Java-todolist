ae55ce343c43852c9cf4d927dd41880e2d2247b1
flameroi <2328Hp210>
2020-10-07 17:10:47 +0300

    Test Init

---
+++ src/test/java/org/example/todolistv2/services/GroupServiceTest.java
@@ -0,0 +1,28 @@
+package org.example.todolistv2.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void found() {
+    }
+
+    @Test
+    void testFound() {
+    }
+}
---
+++ src/test/java/org/example/todolistv2/services/ItemServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.todolistv2.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void found() {
+    }
+
+    @Test
+    void getInfo() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void removeByGroupId() {
+    }
+}
